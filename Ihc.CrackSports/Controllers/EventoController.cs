b9b92cca4e0d601106f494f5ace4ae29c6f90beb
Igor Helio Martins de Carvalho <igor.carvalho@7comm.com.br>
2023-10-22 21:13:55 -0300

    updated

--- Ihc.CrackSports/Controllers/EventoController.cs
+++ Ihc.CrackSports/Controllers/EventoController.cs
@@ -11,88 +11,88 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Ihc.CrackSports.WebApp.Controllers
 {
-    public class EventoController : ControllerBase
-    {
-        private readonly IEventoApplication _eventoApplication;
+	public class EventoController : ControllerBase
+	{
+		private readonly IEventoApplication _eventoApplication;
 
-        public EventoController(IClubService clubService, IAlunoService alunoService, UserManager<Usuario> userManager, INotificationCommand notificationCommand,
-            IUsuarioContext httpContextAccessor, IEventoApplication eventoService) : base(clubService, alunoService, userManager, notificationCommand, httpContextAccessor)
-        {
-            _eventoApplication = eventoService;
-        }
-
-
-        [HttpGet]
-        public async Task<IActionResult> Index()
-        {
-            var de = new DateTime();
-            var ate = new DateTime();
-
-            var result = await _eventoApplication.GetEventos(de, ate);
-
-            return View(result);
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> CadastroEvento(long idEvento)
-        {
-
-            if ((User?.IsAuthenticated() ?? false) && (User.IsClub() || User.IsAdm()))
-            {
-                var evento = idEvento > 0 ? await _eventoApplication.GetEventoById(idEvento) : new Evento();
-                return View(evento);
-            }
-            else
-                return View("Unauthorized");
-
-        }
+		public EventoController(IClubService clubService, IAlunoService alunoService, UserManager<Usuario> userManager, INotificationCommand notificationCommand,
+			IUsuarioContext httpContextAccessor, IEventoApplication eventoService) : base(clubService, alunoService, userManager, notificationCommand, httpContextAccessor)
+		{
+			_eventoApplication = eventoService;
+		}
 
 
-        [HttpPost]
-        public async Task<IActionResult> SalvarEvento(Evento evento)
-        {
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			var de = new DateTime();
+			var ate = new DateTime();
 
-            if (await _eventoApplication.Salvar(evento))
-            {               
-                return View("CadastroEvento", evento);
-            }
+			var result = await _eventoApplication.GetEventos(de, ate);
 
-            throw new ApplicationException("Erro ao salvar o evento. Por favor, tente mais tarde.");
-        }
+			return View(result);
+		}
 
-        [HttpPost]
-        public async Task<PartialViewResult> ListarEventosPartialView([FromBody] ListarEventosRequest model)
-        {
-            DateTime de = new DateTime(), ate = new DateTime();
+		[HttpGet]
+		public async Task<IActionResult> CadastroEvento()
+		{
 
-            if (model.IsValido())
-            {
-                de = DateTime.Parse($"01/{model.Mes}/{model.Ano}");
-                ate = de.AddMonths(1).AddDays(-1);
-            }
+			if ((User?.IsAuthenticated() ?? false) && (User.IsClub() || User.IsAdm()))
+			{
+				var evento = new Evento();
+				return View(evento);
+			}
+			else
+				return View("Unauthorized");
 
-
-            var result = await _eventoApplication.GetEventos(de, ate);
-
-            return PartialView("Partial/AgendaEventos/_EventosPartial", result);
-        }
+		}
 
 
-        [HttpGet]
-        public async Task<IActionResult> ConfimarExclusaoEvento(long idEvento)
-        {
-            var evento = await _eventoApplication.GetEventoById(idEvento);
+		[HttpPost]
+		public async Task<IActionResult> SalvarEvento(Evento evento)
+		{
 
-            return View("Partial/AgendaEventos/Partial/_ModalConfirmExclusaoEvento", evento);
-        }
+			if (await _eventoApplication.Salvar(evento))
+			{
+				return View("CadastroEvento", evento);
+			}
+
+			throw new ApplicationException("Erro ao salvar o evento. Por favor, tente mais tarde.");
+		}
+
+		[HttpPost]
+		public async Task<PartialViewResult> ListarEventosPartialView([FromBody] ListarEventosRequest model)
+		{
+			DateTime de = new DateTime(), ate = new DateTime();
+
+			if (model.IsValido())
+			{
+				de = DateTime.Parse($"01/{model.Mes}/{model.Ano}");
+				ate = de.AddMonths(1).AddDays(-1);
+			}
 
 
-        [HttpPost]
-        public async Task<IActionResult> ConfimarExclusaoEvento([FromBody] Evento evento)
-        {
-            var result = await _eventoApplication.ExcluirEvento(evento.IdEvento);
+			var result = await _eventoApplication.GetEventos(de, ate);
 
-            return Ok(result);
-        }
-    }
+			return PartialView("Partial/AgendaEventos/_EventosPartial", result);
+		}
+
+
+		[HttpGet]
+		public async Task<IActionResult> ConfimarExclusaoEvento(long idEvento)
+		{
+			var evento = await _eventoApplication.GetEventoById(idEvento);
+
+			return View("Partial/AgendaEventos/Partial/_ModalConfirmExclusaoEvento", evento);
+		}
+
+
+		[HttpPost]
+		public async Task<IActionResult> ConfimarExclusaoEvento([FromBody] Evento evento)
+		{
+			var result = await _eventoApplication.ExcluirEvento(evento.IdEvento);
+
+			return Ok(result);
+		}
+	}
 }
