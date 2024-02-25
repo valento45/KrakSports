631f6e804dde0d79faa314b69cf95002dad205d7
Igor Carvalho <iggor1935@hotmail.com>
2024-02-25 00:05:35 -0300

    Ajustes

---
+++ Ihc.CrackSports/Controllers/RankingController.cs
@@ -0,0 +1,24 @@
+﻿using Ihc.CrackSports.Core.Authorization;
+using Ihc.CrackSports.Core.Authorization.Context.Interfaces;
+using Ihc.CrackSports.Core.Commands.Interfaces;
+using Ihc.CrackSports.Core.Services.Interfaces;
+using Ihc.CrackSports.WebApp.Application.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ihc.CrackSports.WebApp.Controllers
+{
+    public class RankingController : ControllerBase
+    {
+        public RankingController(IClubService clubService, IAlunoService alunoService, UserManager<Usuario> userManager, INotificationCommand notificationCommand, IUsuarioContext httpContextAccessor, IMessageApplication messageApplication)
+            : base(clubService, alunoService, userManager, notificationCommand, httpContextAccessor, messageApplication)
+        {
+        }
+
+        [HttpGet]
+        public IActionResult RankingAtletas()
+        {
+            return View();
+        }
+    }
+}
